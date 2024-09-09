b76ab32f1f91141bbe9e1c86d67ac5c6e2fb28b6
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-09 18:45:12 +0200

    final

---
+++ ControleStructuren-2---Cursusgeld/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ControleStructuren_2___Cursusgeld
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ControleStructuren-2---Cursusgeld/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControleStructuren_2___Cursusgeld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Zet variabelen hier als je die nodig hebt over meerdere functies
+        private int year;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void numericButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Resultaat wordt geschreven in jaar variabele.
+            // TryParse geeft een bool terug (true als het de tekst kan omzetten naarint, 
+            // false als dit niet gaat)
+            bool isNumeric = int.TryParse(yearTextBox.Text, out year);
+
+
+            if (isNumeric)
+            {
+                numericLabel.Content = "Is numeriek";
+                // Indien numeriek, dan kunnen we berekenen
+                calculateButton.IsEnabled = true;
+            }
+            else
+            {
+                numericLabel.Content = "Geef een correct jaartal!";
+                calculateButton.IsEnabled = false;
+            }
+
+            // Verleg de focus naar de BtnBerekenen button
+            calculateButton.Focus();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            const float pricePerCredit = 1.5f;
+            int numberOfClassHours = int.Parse(numberOfClassHoursTextBox.Text);
+
+            // Opnieuw het jaar opvragen omdat het gewijzigd kan zijn.      
+            bool isNumeric = int.TryParse(yearTextBox.Text, out year);
+
+            // Schrikkeljaar: jaar dat deelbaar is door 4 en geen vol eeuwjaar, 
+            // of als het een eeuwjaar is dan moet het deelbaar zijn door 400.     
+            // Dit betekent dat 1900 en 2100 geen schrikkeljaren zijn (volle eeuwjaren) en het jaar 2000 wel (deelbaar door 400). 
+
+            if (isNumeric)
+            {
+                // Test op schrikkeljaar     
+                // * Jaar deelbaar door 4 EN geen eeuwjaar (niet deelbaar door honderd)
+                // * OF een jaar is deelbaar door 400
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                {
+                    leapYearLabel.Content = "Is een schrikkeljaar";
+                    // Schrikkeljaar, dus 8 extra studiepunten
+                    float subscriptionFee = ((numberOfClassHours + 1) * pricePerCredit);
+                    subscriptionFeeTextBox.Text = subscriptionFee.ToString();
+                }
+                else
+                {
+                    leapYearLabel.Content = "Is geen schrikkeljaar";
+                    // Geen schrikkeljaar, dus geen extra studiepunten
+                    subscriptionFeeTextBox.Text = (numberOfClassHours * pricePerCredit).ToString();
+                }
+            }
+
+            yearTextBox.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
